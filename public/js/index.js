22577e8ab6a570cdf17aee91adf4f3db66d3415c
Illich Rada <illich570@gmail.com>
2020-05-28 21:33:35 -0400

    [ADD] fetch from API added

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,25 @@
-const Button = document.getElementById('button');
+const Button = document.getElementById("button");
 
-Button.addEventListener('click',async () =>{
-  let response = await fetch('/api/whoami');
+const getDataUser = async () => {
+  let response = await fetch("/api/whoami");
   let data = await response.json();
-  console.log(data);
-})
+  return data;
+};
+
+const sendUserInfo= async () =>{
+  const response = await getDataUser();
+  console.log(response);
+  document.getElementById("result").innerHTML = `
+  <p>Direccion IP: ${response.ipaddress} </p>
+  <p>Lenguajes: </p/>
+  <p>${response.language}</p>
+  <p>User agent: </p>
+  <p> ${response.software}</p>`;
+}
+
+
+
+
+Button.addEventListener('click',sendUserInfo);
+
+
